fecc6099934438a88911378ae0e599163512272f
Phil Steinke <phil@tgrrr.com>
2021-05-23 17:28:18 +1000

    update to cookiecutter format (very basic)

---
+++ 3725547_3904222/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ml analysis of cervical cancer dataset practical data science assignment 02',
+    author='botbotdot.com',
+    license='',
+)
